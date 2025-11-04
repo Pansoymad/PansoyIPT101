1e67d559d014615c54361d851822eaf983b3fa99
Pansoymad <143574388+Pansoymad@users.noreply.github.com>
2025-11-04 18:53:03 +0800

    Add files via upload

---
+++ PansoyIPT101/PansoyDomain/Commands/IUpdateYouTubeViewerCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PansoyIPT101Solution.PansoyDomain.Models;
+
+namespace PansoyIPT101Solution.PansoyDomain.Commands
+{
+    public interface IUpdateYouTubeViewerCommand
+    {
+        Task Execute(YouTubeViewer youTubeViewer);
+    }
+}
