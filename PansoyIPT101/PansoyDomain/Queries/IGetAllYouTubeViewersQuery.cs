6d77a4bf8cfd06804c5790ac529d4711c6629816
Pansoymad <143574388+Pansoymad@users.noreply.github.com>
2025-11-18 10:16:06 +0800

    Add files via upload

---
+++ PansoyIPT101/PansoyDomain/Queries/IGetAllYouTubeViewersQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PansoyIPT101Solution.PansoyDomain.Models;
+
+namespace PansoyIPT101Solution.PansoyDomain.Queries
+{
+    public interface IGetAllYouTubeViewersQuery
+    {
+        Task<IEnumerable<YouTubeViewer>> Execute();
+    }
+}
